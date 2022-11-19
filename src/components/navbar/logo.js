579cc7235bd030236c55d3dc4a4e3f05e2b09bac
omarkhan03 <omarahmadkhan2003@gmail.com>
2022-11-19 13:15:10 -0700

    Put logo on navbar

--- src/components/navbar/logo.js
+++ src/components/navbar/logo.js
@@ -1,33 +1,10 @@
-import { Heading } from "@chakra-ui/react";
+import { ReactComponent as LogoImage } from './power-thru-logo.svg';
 
 export default function Logo({
   shiftX,
   shiftY,
 }) {
   return (
-    <Heading
-      position={"absolute"}
-      left={shiftX}
-      top={shiftY}
-      // overflow="hidden"
-      cursor={"pointer"}
-      whiteSpace={"nowrap"}
-      as="h1"
-      size="lg"
-      letterSpacing="tighter"
-      py={2}
-      px={4}
-      // bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
-      onClick={() => {
-        if (document) {
-          const _id_ = document.getElementById("home");
-          if (_id_) {
-            _id_.scrollIntoView({ behavior: "smooth" });
-          }
-        }
-      }}
-    >
-      Rami Maalouf
-    </Heading>
+    <LogoImage className="z-100000 w-40 h-10 p-0 m-0"/>
   );
 }
