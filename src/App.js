4cc4a7c46d6af344b0aa997456be75dc3f7d3ee3
omarkhan03 <omarahmadkhan2003@gmail.com>
2022-11-19 10:41:17 -0700

    Created timer and hooked it up with local data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Timer />
     </div>
   );
 }
---
+++ src/Timer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+import { useStopwatch } from 'react-timer-hook';
+
+export default function Timer() {
+
+    const current = new Date()  // current date
+    const lastRelapse = Date.parse(localStorage.getItem('date')) // date of last relapse
+    
+    var difference = (current - lastRelapse) / 1000; // seconds since last relapse
+    
+    const stopwatchOffset = new Date(); 
+    stopwatchOffset.setSeconds(stopwatchOffset.getSeconds() + difference); // setting the offset for the timer
+
+    const {
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+        start,
+        pause,
+        reset,
+    } = useStopwatch({ autoStart: true, offsetTimestamp: stopwatchOffset });
+
+    const relapse = () => 
+    {
+        localStorage.setItem('date', new Date())
+        reset()
+    }
+
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>TIME SINCE LAST RELAPSE:</h1>
+            <div style={{fontSize: '100px'}}>
+                <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+            </div>
+            <button onClick={relapse}>RELAPSE</button>
+        </div>
+  );
+}
