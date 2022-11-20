26c8888769717a2f847ffb4cef9ee0a20e4b268e
omarkhan03 <omarahmadkhan2003@gmail.com>
2022-11-19 19:59:49 -0700

    Added some info to the sections and other details

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,38 @@
 export default function About() {
   return (
-    <section id="about" as="section" pt={8} pb={8} className="h-screen">
-      <h1 as="h2" fontSize="4xl" fontWeight="bold" textAlign="center" mb={4}>
-        About
-      </h1>
-      <p as="p" fontSize="xl" textAlign="center">
-        Power Thru is a 24-hour hackathon hosted by the University of Texas at
-        Austin. It is a great opportunity for students to learn new skills,
-        collaborate with others, and build something awesome. Power Thru is
-        completely free to attend and open to all students. We hope to see you
-        there!
-      </p>
+    <section id="about" className="about h-screen">
+      <div className="content">
+          <div className="content-title">
+              About
+          </div>
+
+          <div className="content-text">
+              POWER THRU is a tool that aims to support those struggling with compulsive addiction to
+              pornographic content. This content is extremely addictive and it can have a severe negative
+              impact on the mental and spiritual health of those who are inflicted. POWER THRU tracks a 
+              user's "streak" and visualizes it in an aesthetically pleasing way to induce a calming effect 
+              on the user and counterract the negative stigma that surrounds this topic.
+              
+              <br/> <br/>
+
+              To set a streak ...
+
+              <br/> <br/>
+
+              We have also provided ...
+
+              <br/> <br/>
+
+              This was developed for Code The Change 2022, which addresses the prompt of creating a
+              software to promote healthy and sustainable living for general populations. {" "}
+
+              <a 
+                href="https://github.com/psycho-baller/nofap" 
+                target="_blank"
+                className="underline">
+                Click here to view the source code for this project.</a>
+          </div>
+      </div>
     </section>
   );
 }
