501777c7d76e3d7efdf8a1a1c50068c3a255a3a2
OsamaBamatraf <60716440+OsamaBamatraf@users.noreply.github.com>
2022-11-19 18:39:26 -0700

    TorusMagic
    Magic

--- src/components/three-js/threeTimer.js
+++ src/components/three-js/threeTimer.js
@@ -1,15 +1,18 @@
+import { useControls } from 'leva'
 import {Canvas} from "@react-three/fiber";
 
 export default function Timer() {
+const { radius, tube, radialSegments, tubularSegments, arc } = useControls({radius: 1, tube: 0.4, radialSegments: 8, tubularSegments: 6,  arc: Math.PI * 2 }, )
+   
     
     return(
         <Canvas>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <mesh>
-                <boxGeometry args={[10, 10, 1]} />
-                <meshStandardMaterial color="hotpink" />
-            </mesh>
+            <mesh >
+            <torusGeometry args={ [ radius , tube , radialSegments , tubularSegments , arc ] } />
+            <meshStandardMaterial  color="mediumpurple" />
+        </mesh>
         </Canvas>
     );
 }
